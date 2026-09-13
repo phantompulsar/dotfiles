8d066203d763f788318154573d957387cd45582c
phantompulsar <317873744+phantompulsar@users.noreply.github.com>
2026-09-13 21:13:38 +1000

    nvim and ghostty color change

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,10 @@
 return {
-  'projekt0n/github-nvim-theme',
-  name = 'github_dark_high_contrast',
-  lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000, -- make sure to load this before all the other start plugins
-  config = function()
-    require('github-theme').setup({
-      -- ...
-    })
-
-    vim.cmd('colorscheme github_dark_high_contrast')
-  end,
+    "oxfist/night-owl.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- load the colorscheme here
+      require("night-owl").setup()
+      vim.cmd.colorscheme("night-owl")
+    end,
 }
